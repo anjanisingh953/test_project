c61797649c137ca9c6cffb5cc169d35fa59dde53
Anjani Singh <anjanisingh953@gmail.com>
2025-08-06 16:07:46 +0530

    third commit for feature1

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ console.log('Learn nodejs')
 console.log('Master javascript and nodejs')
 console.log('Node has enriched features are available')
 console.log('Express is best framework for the nodejs');
+console.log('Express helps us to create custom error class');
